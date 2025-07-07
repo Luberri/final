63dcecb7133c24c76348e0e3a6bf2cb26c8725b2
fanama14 <fanarakotomalala@gmail.com>
2025-07-07 15:16:19 +0300

    premier commit

--- ws/index.php
+++ ws/index.php
@@ -1,6 +1,17 @@
 <?php
 require 'vendor/autoload.php';
 require 'db.php';
+
+// Headers CORS
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+
+// Gérer les requêtes OPTIONS (preflight)
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    exit(0);
+}
+
 require 'routes/etudiant_routes.php';
 
 Flight::start();
