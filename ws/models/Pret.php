6458e5f7d0119bd98df9c8672819b0079b23d094
IotyRandrianoely <ioty.ratisbonne@gmail.com>
2025-07-07 20:32:40 +0300

    ee
    e

--- ws/models/Pret.php
+++ ws/models/Pret.php
@@ -4,13 +4,14 @@ require_once __DIR__ . '/../db.php';
 class Pret {
     public static function create($data) {
         $db = getDB();
-        $stmt = $db->prepare("INSERT INTO pret (client_id, montant, duree, type_remboursement_id, type_pret_id) VALUES (?, ?, ?, ?, ?)");
+        $stmt = $db->prepare("INSERT INTO pret (client_id, montant, duree, type_remboursement_id, type_pret_id, assurance) VALUES (?, ?, ?, ?, ?, ?)");
         $stmt->execute([
             $data->client_id,
             $data->montant,
             $data->duree,
             $data->type_remboursement_id,
-            $data->type_pret_id
+            $data->type_pret_id,
+            isset($data->assurance) ? $data->assurance : 0
         ]);
         return $db->lastInsertId();
     }
